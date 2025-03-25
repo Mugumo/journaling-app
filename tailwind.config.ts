9bd4bb4534188c87d90faf3541a5bd4fc6baf333
mugumo <zachary.gakuo@fin.africa>
2025-03-25 13:09:07 +0300

    color pallete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,38 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	darkMode: "class", // Enable dark mode with 'class' strategy
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+				bg: {
+					beige: {
+						dark: "#F5ECE0",
+						light: "#F2DEBA", // Fixed missing `#`
+					},
+				},
+				purple: {
+					accent: "#6C00FF",
+				},
+				primary: {
+					blue: "#3C79F5",
+					green: "#0D7C66", // Fixed missing quotes
+				},
+				secondary: {
+					blue: "#336D82", // Fixed missing quotes
+					green: "#BDE8CA",
+				},
+				accent: "#2DCDDF",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
