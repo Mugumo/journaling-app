600e8c9d31cd01db325012dbd02f721d5d353b03
mugumo <zachary.gakuo@fin.africa>
2025-03-25 11:59:44 +0300

    authenticated paths

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+export { default } from 'next-auth/middleware';
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - login (Auth routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!login|_next/static|images|_next/image|favicon*).*)',
+  ],
+};
