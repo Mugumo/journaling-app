a481e9e0482c4f7a459059695777af942ed273b6
mugumo <zachary.gakuo@fin.africa>
2025-03-26 23:53:13 +0300

    add color to Label model

---
+++ prisma/migrations/20250326140619_remove_description_from_labels/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `Label` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Label" DROP COLUMN "description";
